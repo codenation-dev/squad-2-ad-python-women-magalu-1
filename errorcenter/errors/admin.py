8686f1b05e2d3fe75e5136c4b04fcf4bcf372194
Francielly <franciellydeveloper@gmail.com>
2019-10-15 19:05:04 -0300

    Novo - Criação do Error e do User

--- errorcenter/errors/admin.py
+++ errorcenter/errors/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from .models import Error
+from django.utils.html import format_html
+from django.urls import reverse
+
+from .models import User, Error
 
 
 class ErrorModelAdmin(admin.ModelAdmin):
-    list_display = ('title', 'level', 'environment', 'created_at', 'address')
+    def related_user(self, obj):
+        href = reverse('admin:errors_user_change', args=(obj.user.pk,))
+        return format_html(f'<a href="{ href }">{ obj.user }</a>')
+
+    related_user.short_description = 'Usuário'
+
+    list_display = ('title', 'level', 'environment', 
+                    'created_at', 'address', 'related_user')
 
 
+admin.site.register(User)
 admin.site.register(Error, ErrorModelAdmin)
--- errorcenter/errors/serializers.py
+++ errorcenter/errors/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 
-from .models import Error
+from .models import User, Error
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'password')
 
 
 class ErrorSerializer(serializers.ModelSerializer):
