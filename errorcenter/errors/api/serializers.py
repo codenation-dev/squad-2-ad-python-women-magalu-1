d3344ce6b15b81b5668887a5fee971049b847659
Francielly <franciellydeveloper@gmail.com>
2019-10-29 21:56:24 -0300

    Alteração nome do usuário na tela de detalhes

--- errorcenter/errors/api/serializers.py
+++ errorcenter/errors/api/serializers.py
@@ -6,10 +6,12 @@ from ..models import User, Error
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('email', 'password')
+        fields = ('first_name', 'last_name', 'email', 'password')
 
 
 class ErrorSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Error
         fields = '__all__'
