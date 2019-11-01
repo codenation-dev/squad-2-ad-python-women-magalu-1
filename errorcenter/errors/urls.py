b48b2cb4d0362a0cddc811a2d14abb1a332e1d31
Francielly <franciellydeveloper@gmail.com>
2019-11-01 10:23:26 -0300

    Alteração url login

--- errorcenter/errors/urls.py
+++ errorcenter/errors/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', views.user_login, name='user-login'),
+    path('', views.user_login, name='user-login'),
     path('register/', views.user_register, name='user-register'),
     path('home/', views.error_list, name='home-page'),
     path('logout/', views.deslogar_usuario, name='deslogar-usuario'),
